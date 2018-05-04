396c073695785582aafb3d6936f809f608ed6480
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-04 17:40:23 +0700

    not finish redux saga

--- src/stores/index.js
+++ src/stores/index.js
@@ -4,12 +4,19 @@ import logger from 'redux-logger'
 import articleReducers from './articles/reducers'
 import topArticleReducers from './top-articles/reducers'
 import detailArticleReducers from './detail-article/reducers'
+import createSagaMiddleware from 'redux-saga'
+import mySaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const rootReducer = combineReducers({article: articleReducers, topArticle: topArticleReducers, detailArticle: detailArticleReducers})
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk, logger)
+  applyMiddleware(sagaMiddleware, logger)
 )
+
+sagaMiddleware.run(mySaga)
+
 
 export default store
